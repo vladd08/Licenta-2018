14549e6849310da6caefeee6fc9bfd4de3c8df95
Vlad Orbulescu <vladorbulescu10@gmail.com>
2018-03-18 18:23:49 +0200

    Added heroku deployment and building support, implemented authentication and user roles

---
+++ API/bin/server/controllers/users.js
@@ -0,0 +1,157 @@
+const   
+    express = require('express'),
+    url = require('url'),
+    debug = require('debug')('smart-office-api:server'),
+    parser = require('body-parser'),
+    UnitOfWork = require("../../../helpers/db/UnitOfWorkFactory"),
+    jwt = require('jsonwebtoken'),
+    config = require('../../config'),
+    apiRoutes = require('../../../helpers/middlewares/auth'),
+    UserService = require("../services/userService");
+
+let router = express.Router();
+
+router.get('/', function(req,res){
+    res.json({"Response":"You accessed the users controller!"});
+});
+
+//using the auth middleware
+router.use('/test', apiRoutes);
+router.use('/register', apiRoutes);
+
+router.get('/test', function(req,res,next){
+    if(req.decoded.role != 'admin') return res.status(403).json({
+        'error':'unauthorized',
+        'message':'only admins can access this route.'
+    });
+    let db = UnitOfWork.create((uow) => {
+        if (uow instanceof Error) {
+            next();
+        } else {
+            let data = new UserService(uow);
+            data.getAll((result) => {
+                debug('test result for GET is %s', JSON.stringify(result));
+                res.status(200).json(result);
+                uow.complete()
+            });
+        }
+    });
+});
+
+router.get('/test/:id', function(req,res,next){
+    let db = UnitOfWork.create((uow) => {
+        if (uow instanceof Error) {
+            next();
+        } else {
+            let data = new UserService(uow);
+            let id = req.params.id;
+            data.getById((result, err) => {
+                if(err) {
+                    err.status = 400;
+                    next(err) ;
+                }
+                else res.status(200).json(result);
+                uow.complete();
+            }, id);
+        }
+    });
+});
+
+router.post('/login', function(req,res,next){
+    let db = UnitOfWork.create((uow) => {
+        if (uow instanceof Error) {
+            next();
+        } else {
+            let data = new UserService(uow);
+            let username = req.body.username;
+            let password = req.body.password;
+            data.getByUsernameAndPassword(function(resp){
+                if(resp instanceof Error) {
+                    res.status(401).json({"Error": "Wrong credentials, please try again!"});
+                } else {
+                    var payload = {};
+                    switch(resp.role) {
+                        case 1:
+                        payload = {
+                            role: 'admin',
+                            needsTfaCode: true
+                        };
+                        break;
+                        case 4:
+                        payload = {
+                            role: 'angajat',
+                            needsTfaCode: true
+                        };
+                        break;
+                    }
+                    var token = jwt.sign(payload, config.secret, {
+                        expiresIn: 1440
+                    });
+                    res.status(202).json({
+                        "success:" : "User " + resp.username + " was logged in.",
+                        "token": token
+                    });
+                }
+            }, username, password);
+        }
+    });
+});
+
+router.post('/register', function(req,res,next){
+    if(req.decoded.role != 'admin') return res.status(403).json({
+        'error':'unauthorized',
+        'message':'Only admins can access this route.'
+    });
+    let db = UnitOfWork.create((uow) => {
+        if (uow instanceof Error) {
+            next();
+            // res.status(500).json({'Error' : "Internal server error : could not connect to the database!", "IssuedOn" : new Date()})
+        } else {
+            let data = new UserService(uow);
+            debug('Body params', req.body);
+            if (req.body.email &&
+                req.body.username &&
+                req.body.password &&
+                req.body.sex && 
+                req.body.accessCard) {
+                        var userData = {
+                        email: req.body.email,
+                        username: req.body.username,
+                        password: req.body.password,
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        age: req.body.age,
+                        cnp: req.body.cnp,
+                        sex: req.body.sex,
+                        position: req.body.position,
+                        address: req.body.address,
+                        role: req.body.role,
+                        accessCard: req.body.accessCard,
+                        createdAt : Date.now()
+                    };
+                    debug('User data',userData);
+                    data.insertUser((result,err) => {
+                        if(err) {
+                            err.status = 400;
+                            next(err) ;
+                        }
+                        else {
+                            debug(result);
+                            if(result.errmsg) {
+                                res.status(400).json({error: "There is an account associated with this username / email."});
+                            } else if (res.errors) {
+                                res.status(400).json({error: "Please fill all the required data."});
+                            } else {
+                                res.status(200).json("User " + result.firstname + " " + result.lastname + " was registered successfuly!");
+                            }
+                        }
+                        uow.complete();
+                    }, userData);
+                } else {
+                    return res.status(400).json("Please fill out the form.");
+                }
+            }
+      });
+});
+
+module.exports = router;
